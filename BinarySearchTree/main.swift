8378d1a08d924bd2cb02af9ef5ef1198062afd3f
Gregory Stula <gregstula.dev@icloud.com>
2016-04-22 12:38:12 -0400

    Initial Commit

---
+++ BinarySearchTree/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  BinarySearchTree
+//
+//  Created by Gregory Stula on 4/22/16.
+//  Copyright © 2016 Gregory Stula. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
